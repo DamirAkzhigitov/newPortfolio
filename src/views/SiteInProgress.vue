2c50a4800d013c202af3edad1c80eeae498d08b7
DamirAkzhigitov <damir.ckelet@gmail.com>
2023-06-08 19:58:06 +0300

    Add new view for production

---
+++ src/views/SiteInProgress.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a href='https://github.com/DamirAkzhigitov'>https://github.com/DamirAkzhigitov</a>
+  </div>
+</template>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
