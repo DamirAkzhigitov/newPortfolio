458a085e350f64575d89bbca811e4b9039c7ff81
DamirAkzhigitov <damir.ckelet@gmail.com>
2023-07-07 22:57:10 +0300

    Avatar info layout

--- src/components/AboutView/AvatarInfo.vue
+++ src/components/AboutView/AvatarInfo.vue
@@ -2,8 +2,11 @@
   <section class="avatar-container">
     <ul class="avatar-soft-skill items-end">
       <li v-for="skill in skillsLeft" :key="skill" class="avatar-soft-skill__item text-nowrap">
-        <img :src="skill.icon" class="item-icon" />
-        <span class="item-title">{{ skill.title }}</span>
+        <div v-show="skill.loading" class="item-title v-skeleton-loader__bone"></div>
+        <div v-show="!skill.loading">
+          <img :src="skill?.icon" class="item-icon" />
+          <span class="item-title">{{ skill?.title }}</span>
+        </div>
       </li>
     </ul>
     <div class="profile-container d-flex justify-center items-center flex-column">
@@ -16,51 +19,90 @@
       <div class="profile__name text-bold-large">Damir Akzhigitov</div>
       <div class="profile__location">Limassol, Cyprus</div>
     </div>
-    <ul class="avatar-soft-skill">
+    <ul class="avatar-soft-skill items-end">
       <li v-for="skill in skillsRight" :key="skill" class="avatar-soft-skill__item text-nowrap">
-        <img :src="skill.icon" class="item-icon" />
-        <span class="item-title">{{ skill.title }}</span>
+        <div v-show="skill.loading" class="item-title v-skeleton-loader__bone"></div>
+        <div v-show="!skill.loading">
+          <img :src="skill?.icon" class="item-icon" />
+          <span class="item-title">{{ skill?.title }}</span>
+        </div>
       </li>
     </ul>
     <div class="avatar-hard-skills items-start">
-      <div class="avatar-hard-skills__item">
-        <img src="/logos/figma-logo.png" class="hard-skills__logo" />
+      <div
+        v-for="skill in hardSkills"
+        :key="skill.alt"
+        class="avatar-hard-skills__item"
+        :style="`background-color: ${skill.color}`"
+      >
+        <img :src="skill.icon" height="24" width="24" class="hard-skills__logo" />
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
-const skillsLeft = ref([
+const format = (str) => str.toLowerCase().replace(/[^a-zA-Z\d|&]/g, '_')
+
+const getIcon = (name) => `/logos/${name}.svg`
+
+const loading = ref(true)
+
+const loadingsSkills = [
   {
-    icon: '/logos/javascript.svg',
-    title: 'Javascript'
+    loading: true
   },
   {
-    icon: '/logos/aws-amplify.svg',
-    title: 'AWS amplify'
+    loading: true
   },
   {
-    icon: '/logos/vuetify.svg',
-    title: 'Vuetify'
+    loading: true
+  }
+]
+
+const skillsLeft = ref(loadingsSkills)
+const skillsRight = ref(loadingsSkills)
+
+const hardSkills = ref([
+  {
+    icon: '/logos/git.svg',
+    alt: 'git',
+    color: 'rgba(240, 60, 46, 0.1);'
   }
 ])
-const skillsRight = ref([
-  {
-    icon: '/logos/html.svg',
-    title: 'HTML 5'
-  },
-  {
-    icon: '/logos/network-connection.svg',
-    title: 'GraphQL & REST'
-  },
-  {
-    icon: '/logos/scss.svg',
-    title: 'Sass'
+
+const getSkills = async () => {
+  const data = await fetch('/api/skills')
+
+  const response = await data.json()
+
+  if (Array.isArray(response) && response.length > 0) {
+    const skillsResponse = response[0]
+
+    const { SS: skillList } = skillsResponse.skills
+
+    const rightSide = skillList.map((item) => {
+      const iconName = format(item)
+
+      return {
+        title: item,
+        icon: getIcon(iconName)
+      }
+    })
+
+    skillsLeft.value = rightSide.splice(0, 3)
+    skillsRight.value = rightSide
+
+    // loading.value = false
+    console.log('skillsLeft: ', skillsLeft.value)
   }
-])
+}
+
+onMounted(() => {
+  getSkills()
+})
 </script>
 
 <style lang="scss">
@@ -69,7 +111,7 @@ $shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.2);
 .avatar-container {
   display: grid;
   grid-template-rows: 1fr 60px;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 15px;
 
   max-width: 700px;
@@ -80,7 +122,6 @@ $shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.2);
     box-shadow: $shadow;
     border-radius: var(--space-m);
     padding: var(--space-m);
-    min-width: 250px;
 
     .profile-picture {
       &__image {
@@ -104,12 +145,19 @@ $shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.2);
         padding: 0 var(--space-xs);
       }
     }
+
+    @media screen and (max-width: 600px) {
+      .profile-picture__image {
+        height: 140px;
+      }
+    }
   }
 
   .avatar-soft-skill {
     display: flex;
     flex-flow: column;
     justify-content: center;
+    align-items: center;
     padding: 0;
     font-family: 'JetBrains Mono', monospace;
     user-select: none;
@@ -120,6 +168,7 @@ $shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.2);
       margin-right: var(--space-s);
     }
     &__item {
+      width: fit-content;
       padding: var(--space-sm) var(--space-m);
       display: flex;
       align-items: center;
@@ -133,6 +182,22 @@ $shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.2);
 
       &:hover {
         box-shadow: 5px 5px 35px -15px rgba(0, 0, 0, 0.5);
+      }
+    }
+    .item-title.v-skeleton-loader__bone {
+      width: 100px;
+      height: 20px;
+    }
+
+    @media screen and (max-width: 600px) {
+      .avatar-soft-skill__item {
+        box-shadow: 5px 5px 25px -5px rgba(0, 0, 0, 0.22);
+      }
+      .item-title {
+        display: none;
+      }
+      .item-icon {
+        margin-right: 0;
       }
     }
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,28 +1,66 @@
 <template>
-  <div class='about-page'>
-    <avatar-info class='avatar-info' />
-    <short-description class='short-description' />
-    <contact-block class='contact-block'/>
-    <experience-list class='experience-list' />
-    <education-block class='education-block'/>
-    <projects-description class='projects-description' />
-    <skills-list class='skills-list'/>
+  <div class="about-page">
+    <avatar-info class="avatar-info" />
+    <short-description class="short-description" />
+    <contact-block class="contact-block" />
+    <experience-list class="experience-list" />
+    <education-block class="education-block" />
+    <projects-description class="projects-description" />
+    <skills-list class="skills-list" />
   </div>
 </template>
 
-<script setup lang='ts'>
-import { defineAsyncComponent } from 'vue'
+<script setup lang="ts">
+import { defineAsyncComponent, onMounted } from 'vue'
 
-const EducationBlock  = defineAsyncComponent(() => import("@/components/AboutView/EducationBlock.vue"))
-const ContactBlock = defineAsyncComponent(() => import("@/components/AboutView/ContactBlock.vue"))
-const AvatarInfo = defineAsyncComponent(() => import("@/components/AboutView/AvatarInfo.vue"))
-const ProjectsDescription = defineAsyncComponent(() => import("@/components/AboutView/ProjectsDescription.vue"))
-const SkillsList = defineAsyncComponent(() => import("@/components/AboutView/SkillsList.vue"))
-const ShortDescription = defineAsyncComponent(() => import("@/components/AboutView/ShortDescription.vue"))
-const ExperienceList = defineAsyncComponent(() => import("@/components/AboutView/ExperienceList.vue"))
+const API = 'https://6h68rbyn3i.execute-api.eu-north-1.amazonaws.com/dev'
+
+const EducationBlock = defineAsyncComponent(
+  () => import('@/components/AboutView/EducationBlock.vue')
+)
+const ContactBlock = defineAsyncComponent(() => import('@/components/AboutView/ContactBlock.vue'))
+const AvatarInfo = defineAsyncComponent(() => import('@/components/AboutView/AvatarInfo.vue'))
+const ProjectsDescription = defineAsyncComponent(
+  () => import('@/components/AboutView/ProjectsDescription.vue')
+)
+const SkillsList = defineAsyncComponent(() => import('@/components/AboutView/SkillsList.vue'))
+const ShortDescription = defineAsyncComponent(
+  () => import('@/components/AboutView/ShortDescription.vue')
+)
+const ExperienceList = defineAsyncComponent(
+  () => import('@/components/AboutView/ExperienceList.vue')
+)
+
+const callApi = () => {
+  // instantiate a headers object
+  const myHeaders = new Headers()
+  // add content type header to object
+  myHeaders.append('Content-Type', 'application/json')
+  // using built in JSON utility package turn object to string and store in a variable
+  const raw = JSON.stringify({
+    firstName: 'Damir',
+    lastName: 'Testov'
+  })
+  // create a JSON object with parameters for API call and store in a variable
+  const requestOptions: RequestInit = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  }
+  // make API call with parameters and use promises to get response
+  fetch(API, requestOptions)
+    .then((response) => response.text())
+    .then((result) => alert(JSON.parse(result).body))
+    .catch((error) => console.log('error', error))
+}
+
+onMounted(() => {
+  // callApi()
+})
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 .about-page {
   display: grid;
   grid-template-columns: 60% 1fr;
@@ -39,6 +77,11 @@ const ExperienceList = defineAsyncComponent(() => import("@/components/AboutView
 
   & > section {
     padding: var(--space-s);
+  }
+
+  @media screen and (max-width: 600px) {
+    display: flex;
+    flex-flow: column;
   }
 }
 </style>
